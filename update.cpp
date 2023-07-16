135e611f14fec313ab63d52067ec1a8adc07808e
WenJun <nczywq@gmail.com>
2023-07-16 23:02:11 +0800

    Sublime

--- update.cpp
+++ update.cpp
@@ -1,9 +1,26 @@
 #include "update.h"
 
-update::update(QWidget *parent)
+update::update(int argc, char *argv[], QWidget *parent)
     : QMainWindow(parent)
 {
 
+    if(argc != 9){
+        QMessageBox::critical(this,tr("错误"),tr("传入参数错误，程序无法运行"));
+        qApp->exit(0);
+    }
+    sqladdress = argv[1];
+    sqlport = QString(argv[2]).toInt();
+    sqltype = argv[3];
+    sqldatabasename = argv[4];
+    sqlusername = argv[5];
+    sqlpassword = argv[6];
+    sqltablename = argv[7];
+    softwarecurrentversion = argv[8];
+#if 0
+    for(int i = 1 ; i < argc ; i += 1){
+        qDebug() << argv[i];
+    }
+#endif
     QFont font;
     font.setFamily("WenQuanYi Micro Hei");
     //font.setFamily("WenQuanYi Micro Hei Mono");
@@ -12,17 +29,12 @@ update::update(QWidget *parent)
 
     configlayout();
 
-    retranslateUi(this);
+    checkUpdate();
 
-    QFile file(QCoreApplication::applicationDirPath() + "/update/updateinf.txt");
-    if(file.open(QIODevice::ReadOnly | QIODevice::Text)){
-        QByteArray text = file.readAll();
-        texteditUpdateInf->setText(text);
-        file.close();
-    }
-    else{
-        QMessageBox::critical(this,tr("错误"),tr("未发现准备就绪的更新文件，点击确定后退出"));
-    }
+
+
+
+    retranslateUi(this);
 }
 
 update::~update()
@@ -38,11 +50,15 @@ void update::configlayout()
     labelTitle->setFont(mfont);
     labelTitle->setAlignment(Qt::AlignCenter);
 
+    labelSoftWareName = new QLabel(this);
+
     labelNowVersion = new QLabel(this);
 
     labelNewVersion = new QLabel(this);
 
     labelUpdateTime = new QLabel(this);
+
+
 
     texteditUpdateInf = new QTextEdit(this);
     texteditUpdateInf->setReadOnly(true);
@@ -54,13 +70,14 @@ void update::configlayout()
 
     auto vboxall = new QVBoxLayout;
     vboxall->addWidget(labelTitle);
+    vboxall->addWidget(labelSoftWareName);
     vboxall->addWidget(labelNowVersion);
     vboxall->addWidget(labelNewVersion);
     vboxall->addWidget(labelUpdateTime);
     vboxall->addWidget(texteditUpdateInf);
     vboxall->addWidget(progressbarPercent);
 
-    vboxall->addStretch();
+   // vboxall->addStretch();
 
     QWidget * widget = new QWidget;
     widget->setLayout(vboxall);
@@ -71,10 +88,91 @@ void update::retranslateUi(QMainWindow *mainwindow)
 {
     mainwindow->setWindowTitle(tr("自动更新程序"));
     labelTitle->setText(tr("自动更新程序"));
-    labelNowVersion->setText(tr("当前程序版本：") + "1.0.0.0");
-    labelNewVersion->setText(tr("最新程序版本：") + "1.0.0.1");
-    labelUpdateTime->setText(tr("更新时间：") + "202307141521");
+    labelSoftWareName->setText(tr("软件名字：") + "  " + softwarename );
+    labelNowVersion->setText(tr("当前程序版本：") + "  " + softwarecurrentversion);
+    labelNewVersion->setText(tr("最新程序版本：") + "  " + softwarelastversion );
+    labelUpdateTime->setText(tr("更新时间：") + "  " + softwareupdatetime);
     progressbarPercent->setFormat(tr("更新进度：") + "%p%");
+}
+
+void update::checkUpdate()
+{
+    if(db.contains(sqlname)){
+        db = QSqlDatabase::database(sqlname);
+    }
+    else {
+        db = QSqlDatabase::addDatabase(sqltype,sqlname);
+    }
+
+    db.setHostName(sqladdress);
+    db.setPort(sqlport);
+    db.setDatabaseName(sqldatabasename);
+    db.setUserName(sqlusername);
+    db.setPassword(sqlpassword);
+    if(db.open()){
+        //qDebug() << "数据库打开成功";// << db.tables();
+
+        QSqlQuery query(db);
+        QString cmd;
+#if 0
+        cmd = QString("select * from %1").arg(sqltablename);
+        query.exec(cmd);
+        for(int i = 0 ; i < query.record().count() ; i += 1){
+            auto name = query.record().fieldName(i);
+            qDebug() << name;
+        }
+#endif
+#if 0
+        qDebug() << sqltablename;
+        cmd = QString("select * from %1").arg(sqltablename);
+        query.exec(cmd);
+        query.first();
+        for(int i = 0 ; i < 6 ; i+= 1){
+            qDebug() << query.value(i).toString();
+        }
+#endif \
+    //获取最新的软件版本
+        cmd = QString("select name from %1 where versionattribute = 'lastversion'").arg(sqltablename);
+        if(query.exec(cmd)){
+            query.first();
+            softwarename = query.value(0).toString();   //获取软件名字
+            // qDebug() <<  "软件名字:" << query.value(0).toString();
+        }
+        else {
+            qDebug() << query.lastError().text();
+        }
+
+        cmd = QString("select version from %1 where versionattribute='lastversion'").arg(sqltablename);
+        if(query.exec(cmd)){
+            query.first();
+            softwarelastversion = query.value(0).toString();   //获取软件版本
+        }
+
+        cmd = QString("select updateinf from %1 where versionattribute='lastversion'").arg(sqltablename);
+        if(query.exec(cmd)){
+            query.first();
+            softwareupdateinf = query.value(0).toString();   //获取更新内容
+            texteditUpdateInf->setText(softwareupdateinf);
+        }
+
+        cmd = QString("select updatetime from %1 where versionattribute='lastversion'").arg(sqltablename);
+        if(query.exec(cmd)){
+            query.first();
+            softwareupdatetime = query.value(0).toString();   //获取更新时间
+        }
+
+        cmd = QString("select updatetime from %1 where versionattribute='lastversion'").arg(sqltablename);
+        if(query.exec(cmd)){
+            query.first();
+            softwareaddress = query.value(0).toString();   //获取软件存放地址
+        }
+    }
+    else{
+        qDebug() << db.lastError().text();
+        QMessageBox::critical(this,tr("错误"),tr("数据库连接错误,请检查数据库配置"));
+
+        qApp->exit(0);
+    }
 }
 
 void update::changeEvent(QEvent *e)
--- update.h
+++ update.h
@@ -12,32 +12,71 @@
 #include <QProgressBar>
 #include <QCoreApplication>
 #include <QMessageBox>
+#include <QLineEdit>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlRecord>
 
 class update : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    update(QWidget *parent = nullptr);
+    update(int argc, char *argv[],QWidget *parent = nullptr);
     ~update();
 
 
 private:
     //窗体控件
     QLabel * labelTitle = nullptr;      //大标题
+
+    QLabel * labelSoftWareName = nullptr;   //软件名字
+
     QLabel * labelNowVersion = nullptr; //当前程序版本
+
     QLabel * labelNewVersion = nullptr; //最新程序版本
+
     QLabel * labelUpdateTime = nullptr; //更新时间
+
     QTextEdit * texteditUpdateInf = nullptr;    //更新内容
 
     QProgressBar * progressbarPercent = nullptr;//进度条
+
+private:
+    QString sqladdress = "";        //mysql数据库地址
+    int sqlport = 0;                //mysql数据库端口
+    QString sqltype = "";           //数据库类型
+    QString sqldatabasename= "";    //mysql数据库名字
+    QString sqlusername = "";       //mysql用户名
+    QString sqlpassword = "";       //mysql密码
+    QString sqltablename = "";      //mysql表格名字
+    QString softwarecurrentversion = "";    //当前软件版本
+
+    QString softwarename = "";      //要更新的软件名字
+    QString softwarelastversion = "";   //最新软件版本
+    QString softwareupdateinf = "";     //更新内容
+    QString softwareupdatetime = "";    //软件更新时间
+    QString softwareaddress = "";       //软件下载地址
+
+    QSqlDatabase db;
+    QString sqlname = "databaseupgrade";
 
 private:
     /**
      * @brief configlayout  界面布局
      */
     void configlayout(void);
+    /**
+     * @brief retranslateUi 界面翻译
+     * @param mainwindow
+     */
     void retranslateUi(QMainWindow * mainwindow);
+    /**
+     * @brief checkUpdate   检查更新
+     */
+    void checkUpdate(void);
 
 protected:
     void changeEvent(QEvent * e);
