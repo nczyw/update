6d7ca36055df1140ebc1dcbd4dedb85e76ee8f38
WenJun <nczywq@qq.com>
2023-08-14 10:36:35 +0800

    Sublime

--- update.cpp
+++ update.cpp
@@ -2,6 +2,7 @@
 update::update(int argc, char *argv[], QWidget *parent)
     : QMainWindow(parent)
 {
+    //后面参数改为--传递
     if(argc != 9 && argc != 2){ //只传递软件版本，使用默认参数工作
         qDebug() << argc;
         QMessageBox::critical(this,tr("错误"),tr("传入参数错误，程序无法运行"));
